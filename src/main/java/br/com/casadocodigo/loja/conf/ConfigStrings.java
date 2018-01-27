1e38056ba4340cdf88d531ab828c10e7ecc35e6a
Gabriel Tavares <gabriel.tavares99@hotmail.com>
2018-01-27 09:31:58 -0200

    Criando classe de configuração - [Temporário]

---
+++ src/main/java/br/com/casadocodigo/loja/conf/ConfigStrings.java
@@ -0,0 +1,9 @@
+package br.com.casadocodigo.loja.conf;
+
+public class ConfigStrings {
+
+    public static String DB_USER = "";
+    public static String DB_PASSWORD = "";
+    public static String DB_NAME = "";
+
+}
