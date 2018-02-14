bb99a64226993035c2460d714f2cc369e4f7ed4d
Gabriel Tavares <gabriel.tavares99@hotmail.com>
2018-02-14 19:30:13 -0200

    Melhorando looking and feel da home.jsp

--- src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
@@ -1,20 +1,30 @@
 package br.com.casadocodigo.loja.controllers;
 
+import br.com.casadocodigo.loja.daos.ProdutoDAO;
+import br.com.casadocodigo.loja.models.Produto;
 import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
 
+    @Autowired
+    ProdutoDAO produtoDAO;
+
     private static final Logger logger = Logger.getLogger(HomeController.class);
+
     @RequestMapping("/")
-    public ModelAndView index(){
+    public ModelAndView index() {
         logger.info("INICIO APP");
-        System.out.println("Entrando na home da CDC");
-        return new ModelAndView("home");
+        List<Produto> produtos = produtoDAO.listar();
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("produtos", produtos);
+        return modelAndView;
     }
 
 }
